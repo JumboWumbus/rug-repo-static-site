903d6e977f41141596809a74b2035c261c3fe4c2
JumboWumbus <stepabovetheeggs@protonmail.com>
2022-03-19 18:14:45 +0000

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
+	"./pages/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     colors: {
